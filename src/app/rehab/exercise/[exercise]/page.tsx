56cad79a151787a3c1d2fc1c8dd094f3c01d2edc
Rahul Datta <rahul3datta@gmail.com>
2025-07-20 19:09:39 -0700

    fix ts bugs

---
+++ src/app/rehab/exercise/[exercise]/page.tsx
@@ -0,0 +1,3 @@
+export default function ExercisePage() {
+  return <div>ExercisePage</div>;
+}
--- src/app/rehab/pose/page.tsx
+++ src/app/rehab/pose/page.tsx
@@ -1,5 +1,10 @@
 import { LivePoseEstimation } from "@/components/rehab/LivePostEstimation";
+import { Suspense } from "react";
 
 export default function ExercisePage() {
-  return <LivePoseEstimation />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LivePoseEstimation />
+    </Suspense>
+  );
 }
